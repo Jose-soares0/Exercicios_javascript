2fb711c7ff91f1bfefb8d1a6ebb39ae23821800f
josé soares da silva <josesoaresdasilva006@gmail.com>
2026-04-09 10:35:17 -0300

    Aprendendo sobre comentarios javascript

---
+++ fundamentos/comentario.js
@@ -0,0 +1,11 @@
+//Comentario de uma linha
+console.log("Linha 1");
+/* Comentário de múltiplas linhas
+Linha 2
+Linha 3 */
+console.log("Linha 4");
+console.log('linha 5');
+/*
+*
+*
+*/ //Comentário de múltiplas linhas
