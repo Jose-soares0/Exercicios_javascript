cd189c6c77202f2934f4a7c1ff8f569264fe4192
josé soares da silva <josesoaresdasilva006@gmail.com>
2026-04-09 11:14:35 -0300

    Cuidados com alguns numeros em javascript

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0);
+console.log("20" / 2) // Caraca javascript é uma linguagem inteligente.
+console.log("Show!" * 2);
+console.log(0.1 + 0.7);
+//console.log(10.toString()) da certo em cima do number, mas não da forma literal do numero
+console.log((10.354).toFixed(2))
