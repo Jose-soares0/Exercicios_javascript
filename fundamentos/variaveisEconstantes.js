5f89a798ce8e32ab258cd7ea7045258a7d123c48
josé soares da silva <josesoaresdasilva006@gmail.com>
2026-04-09 10:44:56 -0300

    O basico de Var let e const

---
+++ fundamentos/variaveisEconstantes.js
@@ -0,0 +1,14 @@
+//O basico de Var let e const
+var a = 3;
+let b = 4; //Criar variaveis usando let, é melhor
+const c = 5; //constante, não pode ser alterada  
+
+var a = 30;
+ b = 40;
+
+console.log(a, b);
+
+a = 300;
+b = 400;
+
+console.log(a, b);
